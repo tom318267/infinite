f59335aee3d856fb928aa7cb70fd916f3681be37
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-08-05 22:11:27 -0400

    add animation to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,28 +9,30 @@ const Home = () => {
       <div className="bg-purple w-screen h-screen">
         <div className="flex flex-col h-screen justify-center items-center text-white">
           <img
-            className="object-contain h-38"
+            className="object-contain h-38 animate__animated animate__bounceInUp"
             src={infinite}
             alt="infinite-logo"
           />
-          <h1 className="text-7xl sm:text-8xl font-alatsi uppercase title">
-            Infinite
-          </h1>
-          <p className="font-cuprum mb-4 text-lg sm:text-2xl">
-            Search for your favorite superheroes!
-          </p>
-          <div className="font-cuprum">
-            <Link to="/register">
-              <button className="py-3 px-5 mr-2 rounded-lg text-lg sm:text-2xl  bg-light_purple">
-                Sign Up
-              </button>
-            </Link>
+          <div className="animate__animated animate__bounceInLeft text-center">
+            <h1 className="text-7xl sm:text-8xl font-alatsi uppercase title">
+              Infinite
+            </h1>
+            <p className="font-cuprum mb-4 text-lg sm:text-2xl">
+              Search for your favorite superheroes!
+            </p>
+            <div className="font-cuprum">
+              <Link to="/register">
+                <button className="py-3 px-5 mr-2 rounded-lg text-lg sm:text-2xl  bg-light_purple">
+                  Sign Up
+                </button>
+              </Link>
 
-            <Link to="/login">
-              <button className="py-3 px-5 rounded-lg text-lg sm:text-2xl bg-light_purple">
-                Login
-              </button>
-            </Link>
+              <Link to="/login">
+                <button className="py-3 px-5 rounded-lg text-lg sm:text-2xl bg-light_purple">
+                  Login
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
